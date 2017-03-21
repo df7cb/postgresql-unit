891daf078bc2e2721532b7445ebee3137e59740b
Christoph Berg <myon@debian.org>
2017-03-21 20:45:32 +0100

    Revert "Throw in more digits to hit 0 K better"
    This reverts commit 20635b9d494071c1a3d58c0aa2f12d662aacb720.
    Turns out it's not needed and doesn't fix the full problem.

--- sql/temperature.sql
+++ sql/temperature.sql
@@ -16,16 +16,16 @@ SELECT '-273.15 degC'::unit, '0 degC'::unit, '1 degC'::unit, 'degC'::unit;
 SELECT '-273.15 degcelsius'::unit, '0 degcelsius'::unit, '1 degcelsius'::unit, 'degcelsius'::unit;
 
 -- Fahrenheit
-SELECT '-459.670000000000016 ℉'::unit, '0 ℉'::unit, '1 ℉'::unit, '℉'::unit;
-SELECT '-459.670000000000016 °F'::unit, '0 °F'::unit, '1 °F'::unit, '°F'::unit;
-SELECT '-459.670000000000016 degF'::unit, '0 degF'::unit, '1 degF'::unit, 'degF'::unit;
-SELECT '-459.670000000000016 degfahrenheit'::unit, '0 degfahrenheit'::unit, '1 degfahrenheit'::unit, 'degfahrenheit'::unit;
+SELECT '-459.67 ℉'::unit, '0 ℉'::unit, '1 ℉'::unit, '℉'::unit;
+SELECT '-459.67 °F'::unit, '0 °F'::unit, '1 °F'::unit, '°F'::unit;
+SELECT '-459.67 degF'::unit, '0 degF'::unit, '1 degF'::unit, 'degF'::unit;
+SELECT '-459.67 degfahrenheit'::unit, '0 degfahrenheit'::unit, '1 degfahrenheit'::unit, 'degfahrenheit'::unit;
 
 -- Rankine
 SELECT '491.67 degR'::unit, '0 degR'::unit, '1 degR'::unit, 'degR'::unit;
 
 -- Réaumur
-SELECT '-218.519999999999982 degreaumur'::unit, '0 degreaumur'::unit, '1 degreaumur'::unit, 'degreaumur'::unit;
+SELECT '-218.52 degreaumur'::unit, '0 degreaumur'::unit, '1 degreaumur'::unit, 'degreaumur'::unit;
 
 SELECT '0 K'::unit @ 'K', '0 K'::unit @ '°C', '0 K'::unit @ '°F', '0 K'::unit @ '°R', '0 K'::unit @ 'degreaumur';
 SELECT '273.15 K'::unit @ 'K', '273.15 K'::unit @ '°C', '273.15 K'::unit @ '°F', '273.15 K'::unit @ '°R', '273.15 K'::unit @ 'degreaumur';
