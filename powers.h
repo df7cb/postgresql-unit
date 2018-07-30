64f4573c6610bed6a15f845eda8b73f75b584093
Christoph Berg <myon@debian.org>
2018-07-30 23:06:06 +0200

    Round 10^{-3,3..21} down to allow for some slack in derived units
    Without this, mobileohm, gammil, waterdensity and abmho show up as 1000
    instead of 1 with a larger prefix.

--- powers.h
+++ powers.h
@@ -3,15 +3,15 @@
 
 #define POWER_27  1e+27                  /*   0x1.9d971e4fe8402p+89  */
 #define POWER_24  0x1.a784379d99db3p+79  /* Y 0x1.a784379d99db4p+79  */
-#define POWER_21  1e+21                  /* Z 0x1.b1ae4d6e2ef5p+69   */
-#define POWER_18  1e+18                  /* E 0x1.bc16d674ec8p+59    */
-#define POWER_15  1e+15                  /* P 0x1.c6bf52634p+49      */
-#define POWER_12  1e+12                  /* T 0x1.d1a94a2p+39        */
-#define POWER_9   1e+09                  /* G 0x1.dcd65p+29          */
-#define POWER_6   1e+06                  /* M 0x1.e848p+19           */
-#define POWER_3   1000                   /* k 0x1.f4p+9              */
+#define POWER_21  0x1.b1ae4d6e2ef4fp+69  /* Z 0x1.b1ae4d6e2ef5p+69   */
+#define POWER_18  0x1.bc16d674ec7ffp+59  /* E 0x1.bc16d674ec8p+59    */
+#define POWER_15  0x1.c6bf52633ffffp+49  /* P 0x1.c6bf52634p+49      */
+#define POWER_12  0x1.d1a94a1ffffffp+39  /* T 0x1.d1a94a2p+39        */
+#define POWER_9   0x1.dcd64ffffffffp+29  /* G 0x1.dcd65p+29          */
+#define POWER_6   0x1.e847fffffffffp+19  /* M 0x1.e848p+19           */
+#define POWER_3   0x1.f3fffffffffffp+9   /* k 0x1.f4p+9              */
 #define POWER_0   1                      /*   0x1p+0                 */
-#define POWER__3  0.001                  /* m 0x1.0624dd2f1a9fcp-10  */
+#define POWER__3  0x1.0624dd2f1a9fbp-10  /* m 0x1.0624dd2f1a9fcp-10  */
 #define POWER__6  0x1.0c6f7a0b5ed8cp-20  /* µ 0x1.0c6f7a0b5ed8dp-20  */
 #define POWER__9  1e-09                  /* n 0x1.12e0be826d695p-30  */
 #define POWER__12 0x1.19799812dea10p-40  /* p 0x1.19799812dea11p-40  */
